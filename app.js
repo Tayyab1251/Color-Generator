4daeb108ed7d5ba0044682a408fc556631aa574b
Tayyab S <141709147+Tayyab1251@users.noreply.github.com>
2024-01-13 22:57:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const getColor = ()=>{
+
+    let randomNumber =Math.floor(Math.random()*16777215);
+
+    // console.log(randomNumber);
+
+    let colorCode = "#" + randomNumber.toString(16);
+
+    // console.log(colorCode)
+
+    document.body.style.backgroundColor = colorCode;
+
+    document.getElementById("color-code-section").innerText=colorCode;
+    
+    // document.getElementById("call-to-action").style.backgroundColor = colorCode;
+}
+getColor();
+
+const copyMe = ()=>{
+
+    let copyText = document.getElementById("color-code-section");
+
+    navigator.clipboard.writeText(copyText.innerText)
+
+    alert("Color code copied !!");
+}
